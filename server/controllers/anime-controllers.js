fd9e268eab4811f5e1ebb34a928c9114aef44e5f
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-14 21:24:14 -0500

    create anime function

--- server/controllers/anime-controllers.js
+++ server/controllers/anime-controllers.js
@@ -1,1 +1,37 @@
-const Anime = require('../models/anime-model')
+const Anime = require("../models/anime-model");
+
+// Create anime 
+createAnime = (req, res) => {
+  const body = req.body;
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide a valid anime!",
+    });
+  }
+
+  const anime = new Anime(body);
+
+  if (!anime) {
+    return res.status(400).json({ success: false, error: err });
+  }
+
+  anime
+    .save()
+    .then(() => {
+      return res.status(201).json({
+        success: true,
+        id: anime._id,
+        message: "Anime created!",
+      });
+    })
+    .catch((error) => {
+      return res.status(400).json({
+        error,
+        message: "Anime not created!",
+      });
+    });
+};
+
+module.exports = createAnime;
