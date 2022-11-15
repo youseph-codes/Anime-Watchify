a68ade7e84b8beff21c1488ece5b454837bc2b4f
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-14 21:29:55 -0500

    update anime function

--- server/controllers/anime-controllers.js
+++ server/controllers/anime-controllers.js
@@ -1,6 +1,6 @@
 const Anime = require("../models/anime-model");
 
-// Create anime 
+// Create anime
 createAnime = (req, res) => {
   const body = req.body;
 
@@ -34,4 +34,43 @@ createAnime = (req, res) => {
     });
 };
 
-module.exports = createAnime;
+// Update anime
+updateAnime = async (req, res) => {
+  const body = req.body;
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide a body to update!",
+    });
+  }
+
+  Anime.findOne({ _id: req.params.id }, (err, anime) => {
+    if (err) {
+      return res.status(404).json({
+        err,
+        message: "Anime not found!",
+      });
+    }
+    anime.name = body.name;
+    anime.time = body.time;
+    anime.rating = body.rating;
+    anime
+      .save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          id: anime._id,
+          message: "Anime updated!",
+        });
+      })
+      .catch((error) => {
+        return res.status(404).json({
+          error,
+          message: "Anime not updated!",
+        });
+      });
+  });
+};
+
+module.exports = { createAnime, updateAnime };
