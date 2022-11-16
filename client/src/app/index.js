4075c8dc5e425bfc311afc313f0100679c94304b
Dante Rodriguez <rodriguezdante89@yahoo.com>
2022-11-15 19:55:22 -0500

    having route issues

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import { NavBar } from '../components'
 import {AnimeList, AnimeInsert, AnimeUpdate} from '../pages'
@@ -10,12 +10,12 @@ function App() {
     return (
         <Router>
             <NavBar />
-            <Switch>
+            <Routes>
                 {/* Adding routes */}
                 <Route path="/anime/list" exact component={AnimeList} />
                 <Route path="/anime/create" exact component={AnimeInsert} />
                 <Route path="anime/update/:id" exact component={AnimeUpdate} />
-            </Switch>
+            </Routes>
         </Router>
     )
 }
---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import AnimeInsert from "./AnimeInsert";
+import AnimeList from "./AnimeList";
+import AnimeUpdate from "./AnimeUpdate";
+
+
+
+export{AnimeInsert, AnimeList, AnimeUpdate};
