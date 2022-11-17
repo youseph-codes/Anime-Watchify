683b2bbba42060d796a075d8766e0b419ef52021
Yousopp <yousephagueye@gmail.com>
2022-11-17 01:07:56 -0500

    initialized const variables in resolver

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,7 @@
+const { User, Anime } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('appolo-server-express');
+
+const resolvers = {
+    
+}
