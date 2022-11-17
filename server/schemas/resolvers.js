ae5c1b92c9a61fcc7788cdbb19c1fabe527646cf
Yousopp <yousephagueye@gmail.com>
2022-11-17 01:25:43 -0500

    began populating resolvers + adding variables to auth.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,5 +3,21 @@ const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('appolo-server-express');
 
 const resolvers = {
-    
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('dislikedMovies')
+                    .populate({
+                       path: 'likedMovies',
+                       populate: {
+                         path: 'likedUsers'
+                       }
+                    });;
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+    }
 }
