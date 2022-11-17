7288a9c64e9954434ec0060561bcdd29026ea02d
Yousopp <yousephagueye@gmail.com>
2022-11-17 01:03:01 -0500

    connected index.js in models

--- server/models/index.js
+++ server/models/index.js
@@ -1,13 +1,4 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const User = require('./User');
+const Anime = require('./Anime');
 
-const Anime = new Schema(
-    {
-        name: { type: String, required: true },
-        time: { type: [String], required: true },
-        rating: { type: Number, required: true },
-    },
-    { timestamps: true },
-)
-
-module.exports = mongoose.model('anime', Anime)
+module.exports = { User, Anime};
