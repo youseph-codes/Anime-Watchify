cb057b068c629d63b05f2533a92c1ee2139dc46e
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-17 13:46:12 -0500

    new file structure

---
+++ server/utils/auth.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
+
+module.exports = {
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.query, or headers
+    // original code
+    // let token = req.body.token || req.query.token || req.headers.authorization;
+
+    let token = req.query.token || req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+    }
+
+    return req;
+  },
+  // add back the _id
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
