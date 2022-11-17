7c3e72066c72cba710d74a5f117f63c13ec14aa9
Yousopp <yousephagueye@gmail.com>
2022-11-17 05:23:34 -0500

    initialized files

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-
 const app = express()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -11,10 +10,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL = "mongodb+srv://yousephCodes:aminaG9700@cluster0.hvijyus.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`We live on port ${PORT}`)))
 .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify', false);
+// mongoose.set('useFindAndModify', false);
