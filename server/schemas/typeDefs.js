92a4e004fcdd48d22a04752fd5deccb6cd6f4f95
Yousopp <yousephagueye@gmail.com>
2022-11-17 01:21:21 -0500

    set up typeDefs file structure

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,32 @@
 const { gql } = require('apollo-server-express');
 const typeDefs =  gql`
-type Auth {
-    token: ID!
-    user: User
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Anime {
+        _id: ID
+        voteCount: Int
+        title: String
+        likedUsers: [User]
+        dislikedUsers: [User]
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+    }
+
+    type Query {
+
+    }
+
+    type Mutation {
+        
+    }
+    }
 `;
 
 module.exports = typeDefs;
